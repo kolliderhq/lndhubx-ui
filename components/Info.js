923cd7abfa09072d2ae445a1840707ab8414d107
the-kollider <info@kollider.xyz>
2022-05-17 13:32:20 +0100

    improved back naviation from info component

--- components/Info.js
+++ components/Info.js
@@ -2,13 +2,22 @@ import { VIEWS } from 'consts';
 import { setView } from 'contexts/modules/layout';
 import { ImArrowLeft2 } from 'react-icons/im';
 import { storeDispatch } from 'contexts';
+import { useAppSelector } from 'hooks';
 
 export const Info = () => {
+	const [isLoggedIn] = useAppSelector(state => [state.connection.isLoggedIn]);
+	const onBack = () => {
+		if (isLoggedIn) {
+			storeDispatch(setView(VIEWS.OVERVIEW))
+		} else {
+			storeDispatch(setView(VIEWS.WELCOME))
+		}
+	}
 	return (
 		<div className="flex flex-col h-full p-8 relative text-black">
 			<div className="flex flex-row w-full text-4xl text-gray-600">
 				<div className="">
-					<button onClick={() => storeDispatch(setView(VIEWS.OVERVIEW))}>
+					<button onClick={() => onBack()}>
 						<ImArrowLeft2 />
 					</button>
 				</div>
@@ -20,8 +29,8 @@ export const Info = () => {
 				</div>
 				<div className="mt-5 font-bold text-lg text-gray-800">What's a synthetic USD account?</div>
 				<p className="text-sm">A synthetic USD account is a short Bitcoin position against the USD dollar, that is settled in Bitcoin.</p>
-				<p className="text-sm mt-2">You cannot spend USD directly. When you send funds the payment will be settled in Bitcoin. However if you send USD through 
-				the UI your synthetic USD will be converted back into Bitcoin at the time of payment.</p>
+				<p className="text-sm mt-2">You cannot spend USD directly. When you send funds the payment will be settled in Bitcoin. However if you send USD through
+					the UI your synthetic USD will be converted back into Bitcoin at the time of payment.</p>
 				<div className="mt-5 font-bold text-lg text-gray-800">What LndhubX isn't</div>
 				<div>
 					<ul class="list-disc px-4 text-sm">
